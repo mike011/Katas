e269681da89e42bfe8d993e809b1081d0b72ccd4
Michael Charland <mike_ch_1@hotmail.com>
2014-11-29 22:09:35 -0500

    soln?

--- RomanNumeralsKata/src/RomanNumerals.java
+++ RomanNumeralsKata/src/RomanNumerals.java
@@ -1,23 +1,29 @@
 public class RomanNumerals {
 
-    public static String roman(int i) {
-        String r = "";
-        while (i > 0) {
-            if (i % 6 == 0) {
-                r += "VI";
-                i -= 6;
-            } else if (i % 5 == 0) {
-                r += "V";
-                i -= 5;
-            } else if (i % 4 == 0) {
-                r += "IV";
-                i -= 4;
-            } else {
-                r += "I";
-                i--;
-            }
-        }
-        return r;
-    }
+	public static String roman(int i) {
+		String r = "";
+		while (i > 0) {
+			if (i % 100 == 10) {
+				r = "X" + r;
+				i -= 10;
+			} else if (i % 10 == 9) {
+				r = "IX" + r;
+				i -= 9;
+			} else if (i % 10 == 6) {
+				r = "VI" + r;
+				i -= 6;
+			} else if (i % 10 == 5) {
+				r += "V";
+				i -= 5;
+			} else if (i % 10 == 4) {
+				r += "IV";
+				i -= 4;
+			} else {
+				r += "I";
+				i--;
+			}
+		}
+		return r;
+	}
 
 }
--- RomanNumeralsKata/src/RomanNumeralsTest.java
+++ RomanNumeralsKata/src/RomanNumeralsTest.java
@@ -44,4 +44,29 @@ public class RomanNumeralsTest {
     public void seven() throws Exception {
         assertEquals("VII", RomanNumerals.roman(7));
     }
+
+	@Test
+	public void eight() throws Exception {
+		assertEquals("VIII", RomanNumerals.roman(8));
+	}
+	
+	@Test
+	public void nine() throws Exception {
+		assertEquals("IX", RomanNumerals.roman(9));
+	}
+
+	@Test
+	public void ten() throws Exception {
+		assertEquals("X", RomanNumerals.roman(10));
+	}
+
+	@Test
+	public void eleven() throws Exception {
+		assertEquals("XI", RomanNumerals.roman(11));
+	}
+	
+	@Test
+	public void fifteen() throws Exception {
+		assertEquals("XV", RomanNumerals.roman(15));
+	}
 }
